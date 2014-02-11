198086b64786e76ff982a22b310954a03a21909c
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2014-02-11 18:06:38 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets, window */
+
+/** Simple extension that adds a "File > Hello World" menu item */
+define(function (require, exports, module) {
+    "use strict";
+    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+    ExtensionUtils.loadStyleSheet(module, "visibleTabs.css");
+});
